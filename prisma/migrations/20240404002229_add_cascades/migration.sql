58090aa3483bc86f94f25ba43f904d7a31abdc6e
MichelleCordeiro <mic321@gmail.com>
2024-04-04 14:50:39 -0300

    fix: rename columns in attendees table in schema, migration and db

--- prisma/migrations/20240404002229_add_cascades/migration.sql
+++ prisma/migrations/20240404002229_add_cascades/migration.sql
@@ -4,11 +4,11 @@ CREATE TABLE "new_attendees" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "name" TEXT NOT NULL,
     "email" TEXT NOT NULL,
-    "create_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "event_id" TEXT NOT NULL,
     CONSTRAINT "attendees_event_id_fkey" FOREIGN KEY ("event_id") REFERENCES "events" ("id") ON DELETE CASCADE ON UPDATE CASCADE
 );
-INSERT INTO "new_attendees" ("create_at", "email", "event_id", "id", "name") SELECT "create_at", "email", "event_id", "id", "name" FROM "attendees";
+INSERT INTO "new_attendees" ("created_at", "email", "event_id", "id", "name") SELECT "created_at", "email", "event_id", "id", "name" FROM "attendees";
 DROP TABLE "attendees";
 ALTER TABLE "new_attendees" RENAME TO "attendees";
 CREATE UNIQUE INDEX "attendees_event_id_email_key" ON "attendees"("event_id", "email");
